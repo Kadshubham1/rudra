f37948b48288a36002e007419fd92eb36a8c5ea5
Shubham Kad <shubhamkad622@gmail.com>
2024-10-19 14:35:20 +0530

    m

--- rudra/settings.py
+++ rudra/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9ki%&8!3@qsn-rwc+%*5s6(@!u-hn2f=zo(tr%m#$u$bnr3h_-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
