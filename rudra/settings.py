90f57091f6fb4e37e0babfb93e4c5cc52ba92038
Shubham Kad <shubhamkad622@gmail.com>
2024-11-09 23:27:31 +0530

    remainig contact

--- rudra/settings.py
+++ rudra/settings.py
@@ -31,6 +31,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS =[
+    'http://rudra-lgrq.onrender.com/'
+]
+
 
 # Application definition
 
@@ -161,3 +165,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_ALLOWED = ["link of onrender"]
