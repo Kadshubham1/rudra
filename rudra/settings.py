2fac6799ccea3e82bf30ffdbcabe167fef71411c
Amol Londhe <122874666+AmolLondhe1234@users.noreply.github.com>
2024-11-13 23:36:16 +0530

    Update settings.py

--- rudra/settings.py
+++ rudra/settings.py
@@ -32,7 +32,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS =[
-    'https://rudra-lgrq.onrender.com'
+    'https://rudra-lgrq.onrender.com',
+    'https://rudrayurvedic.com'
 ]
 
 
